6accaa14e445f8b2d03a4076d37436b23bd5d196
SkyLightToday <yulya.kolechkina83648@yandex.ru>
2022-05-23 20:33:51 +0300

    Ronaldo

---
+++ React_Redux_Base-main/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./img2.jpg" alt="football"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
